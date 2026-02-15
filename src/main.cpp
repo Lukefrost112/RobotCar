2866becc016444f77a4d7073c2141344a0180019
Lukefrost112 <111231238+Lukefrost112@users.noreply.github.com>
2026-02-15 23:31:54 +0200

    Added `readController` and `printController` Helper functions and skeleton of changing the light bar also hoping to be able to implement rumble

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,62 @@
 #include <Arduino.h>
+#include <ps5Controller.h>
 
-// put function declarations here:
-int myFunction(int, int);
+unsigned long lastPrintedMs = 0;
+
+struct ControllerInput {
+  bool connected;
+  int lx;
+  int ly;
+  unsigned int r2;
+  unsigned int l2;
+};
+
+ControllerInput controller;
+
+void readController(ControllerInput &c) {
+  c.connected = ps5.isConnected();
+  
+  c.lx = ps5.LStickX();
+  
+  c.ly = ps5.LStickY();
+  
+  c.l2 = ps5.L2Value();
+  
+  c.r2 = ps5.R2Value();
+  
+}
+
+void printController(const ControllerInput c) {
+  unsigned long now = millis();
+
+  if (now - lastPrintedMs >= 200) {
+    Serial.print("Status: ");
+    Serial.println(c.connected ? "Connected" : "Disconnected");
+    if (c.connected) {
+      Serial.print("R2: ");
+      Serial.print(c.r2);
+      Serial.print("L2: ");
+      Serial.print(c.l2);
+      Serial.print("Left Stick X: ");
+      Serial.println(c.lx);
+    }
+  }
+
+  lastPrintedMs = now;
+}
+
+void setControllerLight(int r, int g, int b) {
+
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  ps5.begin("1a:2b:3c:01:01:01");
+  Serial.println("Ready");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
